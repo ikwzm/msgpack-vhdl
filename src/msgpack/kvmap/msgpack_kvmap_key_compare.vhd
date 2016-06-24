d0b07b55d88200b1d896105c59c226b1a8ddcd7c
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2016-06-24 16:56:11 +0900

    [modify] prepare release 0.2.0

--- src/msgpack/kvmap/msgpack_kvmap_key_compare.vhd
+++ src/msgpack/kvmap/msgpack_kvmap_key_compare.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    msgpack_kvmap_key_compare.vhd
 --!     @brief   MessagePack-KVMap(Key Value Map) Key Compare :
---!     @version 0.1.0
---!     @date    2015/10/19
+--!     @version 0.2.0
+--!     @date    2016/6/24
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2015 Ichiro Kawazome
+--      Copyright (C) 2012-2016 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
